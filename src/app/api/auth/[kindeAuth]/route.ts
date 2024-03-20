98d7bf913c2dc0744c37ae89784295d8effa4a22
Sarthak Mathur <sarthak.mathur@mca.christuniversity.in>
2024-03-21 01:27:05 +0530

    kindeAuth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,10 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export async function GET(
     request: NextRequest,
     { params }: any
-): Promise<void | Response> {
+) {
     const endpoint = params.kindeAuth;
-    const [response] = await Promise.all([handleAuth(request, endpoint)]);
-    return NextResponse.from(response);
+    return handleAuth(request, endpoint)
 }
