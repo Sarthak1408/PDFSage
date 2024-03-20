283f8ba2893a5172f95e59205df6748ef054902c
Sarthak Mathur <sarthak.mathur@mca.christuniversity.in>
2024-03-21 01:53:21 +0530

    kindeAuth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -4,22 +4,25 @@
 import { handleAuth, AuthResponse } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (
-    req: NextApiRequest,
-    res: NextApiResponse
-): Promise<void> => {
+async function handleKindeAuth(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     try {
         const { kindeAuth } = req.query;
 
+        // Await the handleAuth function call
         // @ts-ignore
         const authResponse: AuthResponse = handleAuth(req, kindeAuth);
 
-        if (!(!authResponse || !authResponse.body || !authResponse.status || !authResponse.headers)) {
+        // Check if authResponse is valid
+        if (authResponse && authResponse.body && authResponse.status && authResponse.headers) {
             res.status(authResponse.status).setHeader('Content-Type', 'application/json').send(authResponse.body);
-        } else throw new Error('Invalid response from handleAuth');
+        } else {
+            throw new Error('Invalid response from handleAuth');
+        }
     } catch (error) {
         // Handle errors
         console.error('Error in handleAuth:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
+
+export default handleKindeAuth;
